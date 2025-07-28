d6d07b14565e0e7dcb89ac68ef8090c4e763452a
Abebe Endalamie <141606663+Abebeana@users.noreply.github.com>
2025-07-28 14:34:49 +0300

    update index.tsx

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -4,8 +4,7 @@ import { PageRouteProps } from "@/interface/index";
 
 
 export default function Home() {
-  const router = useRouter()
-
+  const router = useRouter();
   // Imeperative routing with useRouter
   const routeToNextPage  = ({ pageRoute }: PageRouteProps) => {
     router.push(pageRoute, undefined, { shallow: false})
