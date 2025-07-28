e086d72aba359d902251881baa178a76819d5b5b
Abebe Endalamie <141606663+Abebeana@users.noreply.github.com>
2025-07-28 13:16:32 +0300

    App initialized

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Layout from "../components/layouts/Layout";
 
 const index = () => {
   return (
-    <div className="xl w-1/2 font-bold bg-amber-600">index</div>
+    <Layout>
+      <div className="xl w-1/2 font-bold bg-amber-600">index</div>
+    </Layout>
   )
 }
 
